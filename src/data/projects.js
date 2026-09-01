56db3c07e30849a3fac238eff65beb93591d7a11
Aaron Wang <aaronw4932@gmail.com>
2026-09-01 13:11:56 -0400

    add new potential projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,5 +6,21 @@ export const projects = [
         title: "portfolio website",
         description: "barebones portfolio website with questionable UI choices showing off my (lack of) projects",
         url: "https://github.com/Aaron-Wang4932/aaron-wang4932.github.io"
+    },
+    {
+        id: 2,
+        status: "wip",
+        date: new Date().toLocaleDateString('en-CA').replace(/-/g, '.'),
+        title: "gesture macros",
+        description: "(private repo rn) startup desktop application to perform computer macros based on hand gestures to computer webcam",
+        url: ""
+    },
+    {
+        id: 3,
+        status: "wip",
+        date: new Date().toLocaleDateString('en-CA').replace(/-/g, '.'),
+        title: "fluentemojis",
+        description: "(private repo rn) uses fluent UI to create a native windows emoji kitchen experience!",
+        url: ""
     }
 ]
